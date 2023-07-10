ae463a5506bcf7ca79e26ca80b7aead67588fa03
Ruslan A <ruslan.absalyamov@gmail.com>
2023-07-10 23:48:49 +0200

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ source secvars.sh
 
 echo "Deploying to $DEPLOY_HOST..."
 
-scp -r ./dist root@$DEPLOY_HOST:/home/media-ext-bot
+scp -r ./dist/ root@$DEPLOY_HOST:/home/media-ext-bot
 
-ssh root@$DEPLOY_HOST 'sh /home/media-ext-bot/dist/run.sh'
+ssh root@$DEPLOY_HOST 'sh /home/media-ext-bot/run.sh'
